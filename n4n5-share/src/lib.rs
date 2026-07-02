1d63c6d41501748aa0ba9b0529a6c0daea019664
n4n5 <its.just.n4n5@gmail.com>
2026-07-02 16:39:30 +0200

    add

--- n4n5-share/src/lib.rs
+++ n4n5-share/src/lib.rs
@@ -1,6 +1,20 @@
 //! n4n5-share lib
 //! Quick, easy and dirty file transfer
 
+#![warn(clippy::all, rust_2018_idioms)]
+#![deny(
+    missing_docs,
+    clippy::all,
+    clippy::missing_docs_in_private_items,
+    clippy::missing_errors_doc,
+    clippy::missing_panics_doc,
+    clippy::cargo,
+    clippy::unwrap_used,
+    clippy::expect_used,
+    clippy::pedantic
+)]
+#![allow(clippy::multiple_crate_versions)]
+
 pub(crate) mod share;
 
 pub use share::cli_main;
